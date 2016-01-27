b005b84982c6df50f4e9cc012f2c5ddc6d3662b9
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2016-01-27 21:25:38 +0100

    iron router added and used for main page

--- client/siteace.js
+++ client/siteace.js
@@ -1,4 +1,34 @@
 /////
+// routing
+/////
+Router.configure({
+    layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function () {
+    this.render('navbar', {
+        to: "navbar"
+    });
+    this.render('websites', {
+        to: "main"
+    });
+});
+
+Router.route('/website/:_id', function () {
+    this.render('navbar', {
+        to: "navbar"
+    });
+    this.render('website', {
+        to: "main",
+        data: function () {
+            return Websites.findOne({
+                _id: this.params._id
+            })
+        }
+    });
+});
+
+/////
 // account configs
 /////
 Accounts.ui.config({
